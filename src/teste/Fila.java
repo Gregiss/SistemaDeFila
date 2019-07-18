f2952142c224526c5cca6df8437fa87d1ab338b3
PassaUmDollar <46573030+PassaUmDollar@users.noreply.github.com>
2019-07-18 19:49:36 -0300

    Fix

--- src/teste/Fila.java
+++ src/teste/Fila.java
@@ -40,11 +40,16 @@ public class Fila extends javax.swing.JFrame {
         cpNome = new javax.swing.JTextField();
         jButton1 = new javax.swing.JButton();
         jButton2 = new javax.swing.JButton();
+        lbl_ante = new javax.swing.JLabel();
+        lbl_penu = new javax.swing.JLabel();
+        lbl_ult = new javax.swing.JLabel();
+        jPanel1 = new javax.swing.JPanel();
+        listaPessoas = new java.awt.List();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
         labelChamada.setFont(new java.awt.Font("Tahoma", 0, 40)); // NOI18N
-        labelChamada.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        labelChamada.setHorizontalAlignment(javax.swing.SwingConstants.LEFT);
         labelChamada.setText("Chamada");
 
         jLabel2.setFont(new java.awt.Font("Tahoma", 0, 18)); // NOI18N
@@ -65,6 +70,32 @@ public class Fila extends javax.swing.JFrame {
             }
         });
 
+        lbl_ante.setFont(new java.awt.Font("Tahoma", 0, 18)); // NOI18N
+        lbl_ante.setText("(Antepenultimo)");
+
+        lbl_penu.setFont(new java.awt.Font("Tahoma", 0, 18)); // NOI18N
+        lbl_penu.setText("(Penultimo)");
+
+        lbl_ult.setFont(new java.awt.Font("Tahoma", 0, 18)); // NOI18N
+        lbl_ult.setText("(Ultimo)");
+
+        javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
+        jPanel1.setLayout(jPanel1Layout);
+        jPanel1Layout.setHorizontalGroup(
+            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel1Layout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(listaPessoas, javax.swing.GroupLayout.DEFAULT_SIZE, 161, Short.MAX_VALUE)
+                .addContainerGap())
+        );
+        jPanel1Layout.setVerticalGroup(
+            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel1Layout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(listaPessoas, javax.swing.GroupLayout.PREFERRED_SIZE, 317, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+        );
+
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
@@ -72,29 +103,48 @@ public class Fila extends javax.swing.JFrame {
             .addGroup(layout.createSequentialGroup()
                 .addContainerGap()
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addComponent(jButton2, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                     .addComponent(jLabel2, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                     .addComponent(labelChamada, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                     .addGroup(layout.createSequentialGroup()
-                        .addComponent(cpNome, javax.swing.GroupLayout.PREFERRED_SIZE, 163, javax.swing.GroupLayout.PREFERRED_SIZE)
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                        .addComponent(jButton1)))
-                .addContainerGap())
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING, false)
+                                .addComponent(jButton2, javax.swing.GroupLayout.Alignment.LEADING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                                .addGroup(layout.createSequentialGroup()
+                                    .addComponent(lbl_ante)
+                                    .addGap(148, 148, 148)))
+                            .addComponent(lbl_penu)
+                            .addComponent(lbl_ult)
+                            .addGroup(layout.createSequentialGroup()
+                                .addComponent(cpNome, javax.swing.GroupLayout.PREFERRED_SIZE, 163, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                .addGap(18, 18, 18)
+                                .addComponent(jButton1)))
+                        .addGap(0, 0, Short.MAX_VALUE)))
+                .addGap(18, 18, 18)
+                .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addGap(4, 4, 4))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
-                .addContainerGap()
+                .addGap(13, 13, 13)
                 .addComponent(jLabel2, javax.swing.GroupLayout.PREFERRED_SIZE, 43, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addComponent(labelChamada, javax.swing.GroupLayout.PREFERRED_SIZE, 103, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addComponent(labelChamada)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(lbl_ante)
+                .addGap(18, 18, 18)
+                .addComponent(lbl_penu)
+                .addGap(26, 26, 26)
+                .addComponent(lbl_ult)
+                .addGap(18, 18, 18)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(cpNome, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                     .addComponent(jButton1))
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                .addComponent(jButton2, javax.swing.GroupLayout.PREFERRED_SIZE, 39, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                .addGap(27, 27, 27)
+                .addComponent(jButton2, javax.swing.GroupLayout.PREFERRED_SIZE, 39, javax.swing.GroupLayout.PREFERRED_SIZE))
+            .addGroup(layout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
         );
 
         pack();
@@ -102,22 +152,43 @@ public class Fila extends javax.swing.JFrame {
 
     private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
        String nome = cpNome.getText().trim();
+       listaPessoas.add(nome);
        cpNome.setText("");
        this.fila.add(nome);
     }//GEN-LAST:event_jButton1ActionPerformed
-
-    
     
     public void chamar(){
         String nome = "";
         int total = this.fila.size();
-        if(total > 0){ 
+        if(total > 0){
             nome = this.fila.get(0);
             this.fila.remove(0);   
-            labelChamada.setText(""+nome);
+            labelChamada.setText(""+nome); 
+            verificar(nome);
+            listaPessoas.removeAll();
+            for(int i = 0; i < this.fila.size(); i++){
+                listaPessoas.add(this.fila.get(i));
+            }
+        } else{
+            listaPessoas.removeAll();
         }
         java.awt.Toolkit.getDefaultToolkit().beep();
     }
+    
+    public int contador = 0;
+    
+    public void verificar(String nome){
+        contador++;
+        if(contador == 1){
+            lbl_ante.setText(nome);
+        } else if(contador == 2){
+            lbl_penu.setText(nome);
+        } else if(contador == 3){
+            lbl_ult.setText(nome);
+            contador = 0;
+        }
+    }
+   
     
     private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
         chamar();
@@ -163,6 +234,11 @@ public class Fila extends javax.swing.JFrame {
     private javax.swing.JButton jButton1;
     private javax.swing.JButton jButton2;
     private javax.swing.JLabel jLabel2;
+    private javax.swing.JPanel jPanel1;
     private javax.swing.JLabel labelChamada;
+    private javax.swing.JLabel lbl_ante;
+    private javax.swing.JLabel lbl_penu;
+    private javax.swing.JLabel lbl_ult;
+    private java.awt.List listaPessoas;
     // End of variables declaration//GEN-END:variables
 }
